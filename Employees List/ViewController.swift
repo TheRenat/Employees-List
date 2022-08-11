8395b5304c6d3d428626ea122a7fa50522af3242
TheRenat <therenat@mail.ru>
2022-08-11 20:33:38 +0300

    Initial Commit

---
+++ Employees List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employees List
+//
+//  Created by Renat Ibragimov on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
